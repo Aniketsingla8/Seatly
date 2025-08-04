1e3dab90e42ebc4442b0db123a204cba85d3c106
Aniket Singla <imaniketsingla8@gmail.com>
2025-08-04 17:37:23 +0530

    Add FeaturedSection component to Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection.jsx'
+import FeaturedSection from '../components/FeaturedSection.jsx'
 
 const Home = () => {
   return (
     <>
       <HeroSection />
+      <FeaturedSection />
     </>
   )
 }
